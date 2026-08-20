2694c178bda2fb7c9e3b159e65ef2a9ad162d099
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 11:02:02 +0200

    Format and require Swift 6.4

--- Package.swift
+++ Package.swift
@@ -1,15 +1,15 @@
-// swift-tools-version: 6.3.3
+// swift-tools-version: 6.4
 
 import PackageDescription
 
 let package = Package(
     name: "swift-random",
     platforms: [
-        .macOS("27"),
-        .iOS("27"),
-        .tvOS("27"),
-        .watchOS("27"),
-        .visionOS("27")
+        .macOS(.v27),
+        .iOS(.v27),
+        .tvOS(.v27),
+        .watchOS(.v27),
+        .visionOS(.v27),
     ],
     products: [
         .library(
@@ -18,25 +18,40 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-primitives/swift-random-primitives.git", branch: "main"),
+        .package(
+            url: "https://github.com/swift-primitives/swift-random-primitives.git",
+            branch: "main"
+        ),
         .package(url: "https://github.com/swift-foundations/swift-darwin.git", branch: "main"),
         .package(url: "https://github.com/swift-foundations/swift-linux.git", branch: "main"),
-        .package(url: "https://github.com/swift-foundations/swift-windows.git", branch: "main")
+        .package(url: "https://github.com/swift-foundations/swift-windows.git", branch: "main"),
     ],
     targets: [
         .target(
             name: "Random",
             dependencies: [
                 .product(name: "Random Primitives", package: "swift-random-primitives"),
-                .product(name: "Darwin Kernel", package: "swift-darwin", condition: .when(platforms: [.macOS, .iOS, .tvOS, .watchOS, .visionOS])),
-                .product(name: "Linux Kernel", package: "swift-linux", condition: .when(platforms: [.linux])),
-                .product(name: "Windows Kernel", package: "swift-windows", condition: .when(platforms: [.windows]))
+                .product(
+                    name: "Darwin Kernel",
+                    package: "swift-darwin",
+                    condition: .when(platforms: [.macOS, .iOS, .tvOS, .watchOS, .visionOS])
+                ),
+                .product(
+                    name: "Linux Kernel",
+                    package: "swift-linux",
+                    condition: .when(platforms: [.linux])
+                ),
+                .product(
+                    name: "Windows Kernel",
+                    package: "swift-windows",
+                    condition: .when(platforms: [.windows])
+                ),
             ]
         ),
         .testTarget(
             name: "Random Tests",
             dependencies: [
-                "Random",
+                "Random"
             ],
             path: "Tests/Random Tests"
         ),
